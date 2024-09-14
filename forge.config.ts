a07d68a23a9b37f39b2a398cfd17f202ad8d5d83
loo-y <loo-y@outlook.com>
2024-09-14 22:02:02 +0800

    feat: set make

--- forge.config.ts
+++ forge.config.ts
@@ -1,6 +1,7 @@
-import type { ForgeConfig } from '@electron-forge/shared-types'
+import type { ForgeConfig, ForgeMakeResult } from '@electron-forge/shared-types'
 import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerZIP } from '@electron-forge/maker-zip'
+import { MakerDMG } from '@electron-forge/maker-dmg'
 import { MakerDeb } from '@electron-forge/maker-deb'
 import { MakerRpm } from '@electron-forge/maker-rpm'
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
@@ -29,11 +30,42 @@ const preBuild = async () => {
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
+        osxSign: {},
     },
     hooks: {
         preMake: async () => {
             await preBuild()
         },
+        // postMake: async (forgeConfig, makeResults): Promise<ForgeMakeResult[]> => {
+        //     const { makeUniversalApp } = require('@electron/universal');
+        //     const path = require('path');
+
+        //     // 找到 macOS 的构建结果
+        //     const macResults = makeResults.filter(result => result.platform === 'darwin');
+
+        //     if (macResults.length >= 2) {
+        //       const x64Result = macResults.find(result => result.arch === 'x64');
+        //       const arm64Result = macResults.find(result => result.arch === 'arm64');
+
+        //       if (x64Result && arm64Result) {
+        //         const universalAppPath = path.join(path.dirname(x64Result.artifacts[0]), '..', 'universal');
+        //         await makeUniversalApp({
+        //           x64AppPath: x64Result.artifacts[0],
+        //           arm64AppPath: arm64Result.artifacts[0],
+        //           outAppPath: path.join(universalAppPath, 'YourAppName.app'),
+        //         });
+
+        //         // 添加通用二进制文件到结果中
+        //         makeResults.push({
+        //           ...x64Result,
+        //           arch: 'universal',
+        //           artifacts: [path.join(universalAppPath, 'YourAppName.app')],
+        //         });
+        //       }
+        //     }
+
+        //     return makeResults;
+        // },
     },
     rebuildConfig: {},
     makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
