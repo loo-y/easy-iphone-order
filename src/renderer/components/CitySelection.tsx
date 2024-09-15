603b3a42329112f2aa05d88656847892136bfbb4
loo-y <loo-y@outlook.com>
2024-09-15 16:40:32 +0800

    feat: add shadcn

--- src/renderer/components/CitySelection.tsx
+++ src/renderer/components/CitySelection.tsx
@@ -1,6 +1,7 @@
 // iPhone 取货门店选择器
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import _ from 'lodash'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 
 const CitySelection: React.FC = () => {
     const [provinceList, setProvinceList] = useState<{ name: string; id: string }[]>([])
@@ -8,6 +9,7 @@ const CitySelection: React.FC = () => {
     const [selectedCityList, setSelectedCityList] = useState<{ name: string; id: string }[] | null>([])
     const [selectedCity, setSelectedCity] = useState<{ name: string; id: string } | null>()
     console.log(`selectedProvince`, selectedProvince?.name, selectedCityList)
+
     useEffect(() => {
         window.electronAPI.getProvinces().then(provinces => {
             setProvinceList(provinces)
@@ -18,62 +20,64 @@ const CitySelection: React.FC = () => {
             })
         }
     }, [])
-    const handleProvinceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const provinceId = event.target.value
+    const handleProvinceChange = (provinceId: string) => {
+        // const provinceId = event.target.value
+        console.log(`provinceId`, provinceId)
         const selectedProvince = _.find(provinceList, province => province.id === provinceId)
         setSelectedProvince(selectedProvince)
         window.electronAPI.getCityList({ provinceId: selectedProvince.id }).then(cityList => {
             console.log(`cityList`, cityList)
             setSelectedCityList(cityList)
+            if (cityList?.length == 1) {
+                setSelectedCity(cityList[0])
+            }
         })
         setSelectedCity(null) // 重置城市选择
     }
 
-    const handleCityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedCityId = event.target.value
+    const handleCityChange = selectedCityId => {
+        // const selectedCityId = event.target.value
         const selectedCity = _.find(selectedCityList, city => city.id === selectedCityId)
         setSelectedCity(selectedCity)
     }
 
     return (
-        <div className="p-4">
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="province">
-                    选择省份
+        <div className="p-4 flex flex-row gap-4">
+            <div className="mb-4 flex flex-col">
+                <label className="ml-1 block text-gray-700 text-sm font-bold mb-2" htmlFor="province_select">
+                    省份
                 </label>
-                <select
-                    id="province"
-                    value={selectedProvince?.id || 0}
-                    onChange={handleProvinceChange}
-                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                >
-                    <option value="">请选择省份</option>
-                    {_.map(provinceList, province => (
-                        <option key={province.name} value={province.id}>
-                            {province.name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
-                    选择城市
-                </label>
-                <select
-                    id="city"
-                    value={selectedCity?.id || 0}
-                    onChange={handleCityChange}
-                    className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                    disabled={_.isEmpty(selectedCityList)}
-                >
-                    <option value="">请选择城市</option>
-                    {selectedCityList &&
-                        _.map(selectedCityList, city => (
-                            <option key={city.id} value={city.id}>
-                                {city.name}
-                            </option>
+                <Select onValueChange={handleProvinceChange}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="选择省份" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {_.map(provinceList, province => (
+                            <SelectItem value={province.id} key={`${province.id}_provincelist`}>
+                                {province.name}
+                            </SelectItem>
                         ))}
-                </select>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div className="mb-4 flex flex-col">
+                <label className="ml-1 block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
+                    城市
+                </label>
+                <Select onValueChange={handleCityChange} value={selectedCity?.id}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="选择城市" />
+                    </SelectTrigger>
+                    {_.isEmpty(selectedCityList) ? null : (
+                        <SelectContent>
+                            {_.map(selectedCityList, city => (
+                                <SelectItem value={city.id} key={`${city.id}_citylist`}>
+                                    {city.name}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    )}
+                </Select>
             </div>
         </div>
     )
